e85f0ba994a1328785bbbc2bb4462a16859f229b
prithwish122 <prithwishchatterjee1277@gmail.com>
2025-09-09 01:04:36 +0530

    fixed text-styles

--- components/magicui/feature-section-demo.tsx
+++ components/magicui/feature-section-demo.tsx
@@ -83,7 +83,7 @@ const FeatureCard = ({
 
 const FeatureTitle = ({ children }: { children?: React.ReactNode }) => {
   return (
-    <p className=" max-w-5xl mx-auto text-left tracking-tight text-black dark:text-white text-xl md:text-2xl md:leading-snug">
+    <p className=" max-w-5xl mx-auto text-left tracking-tight text-3xl">
       {children}
     </p>
   );
@@ -93,7 +93,7 @@ const FeatureDescription = ({ children }: { children?: React.ReactNode }) => {
   return (
     <p
       className={cn(
-        "text-sm md:text-base  max-w-4xl text-left mx-auto",
+        "text-xl tracking-tight max-w-4xl text-left mx-auto",
         "text-neutral-500 text-center font-normal dark:text-neutral-300",
         "text-left max-w-sm mx-0 md:text-sm my-2"
       )}
