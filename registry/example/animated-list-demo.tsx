a39cf692f7bc60a31b6534d1e2fa2d8edf60e822
prithwish122 <prithwishchatterjee1277@gmail.com>
2025-09-08 17:02:06 +0530

    ui changes

--- registry/example/animated-list-demo.tsx
+++ registry/example/animated-list-demo.tsx
@@ -8,7 +8,7 @@ export default function AnimatedListDemo({
 }) {
   return (
     <div className={cn("relative flex max-h-[400px] min-h-[400px] w-full max-w-[32rem] flex-col overflow-hidden rounded-lg border bg-background p-6 shadow-lg", className)}>
-      <AnimatedList />
+      <AnimatedList children={undefined} />
     </div>
   );
 }
